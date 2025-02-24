a764326004815d269c91eeffb0b494d41df59b3c
ernisernis <ernestas.dainys@gmail.com>
2025-02-24 20:55:15 +0200

    Add Hilt entry point, initialize module with ktor client

---
+++ app/src/main/java/com/ernisernis/githubfeed/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.ernisernis.githubfeed.di
+
+import com.ernisernis.githubfeed.core.data.networking.HttpClientFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClientEngine(): HttpClientEngine {
+        return OkHttp.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClientFactory(engine: HttpClientEngine): HttpClient {
+        return HttpClientFactory.create(engine)
+    }
+}
