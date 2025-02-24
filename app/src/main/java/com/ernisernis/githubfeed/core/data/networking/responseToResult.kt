206671f0eb22dd809457dd533d1697c332f7a19d
ernisernis <ernestas.dainys@gmail.com>
2025-02-24 20:25:49 +0200

    Implement core network helper api call functions to catch any HTTP error codes

---
+++ app/src/main/java/com/ernisernis/githubfeed/core/data/networking/responseToResult.kt
@@ -0,0 +1,24 @@
+package com.ernisernis.githubfeed.core.data.networking
+
+import com.ernisernis.githubfeed.core.domain.util.DataError
+import com.ernisernis.githubfeed.core.domain.util.Result
+import io.ktor.client.call.NoTransformationFoundException
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+
+
+suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<T, DataError.Remote> {
+    return when (response.status.value) {
+        in 200..299 -> {
+            try {
+                Result.Success(response.body<T>())
+            } catch (e: NoTransformationFoundException) {
+                Result.Error(DataError.Remote.SERIALIZATION)
+            }
+        }
+        408 -> Result.Error(DataError.Remote.REQUEST_TIMEOUT)
+        429 -> Result.Error(DataError.Remote.TOO_MANY_REQUESTS)
+        in 500..599 -> Result.Error(DataError.Remote.SERVER_ERROR)
+        else -> Result.Error(DataError.Remote.UNKNOWN)
+    }
+}
---
+++ app/src/main/java/com/ernisernis/githubfeed/core/data/networking/safeCall.kt
@@ -0,0 +1,25 @@
+package com.ernisernis.githubfeed.core.data.networking
+
+import com.ernisernis.githubfeed.core.domain.util.DataError
+import com.ernisernis.githubfeed.core.domain.util.Result
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
+import kotlinx.serialization.SerializationException
+import kotlin.coroutines.coroutineContext
+
+suspend inline fun <reified T> safeCall(execute: () -> HttpResponse): Result<T, DataError.Remote> {
+    val response =
+        try {
+            execute()
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(DataError.Remote.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(DataError.Remote.SERIALIZATION)
+        } catch (e: Exception) {
+            coroutineContext.ensureActive()
+            return Result.Error(DataError.Remote.UNKNOWN)
+        }
+
+    return responseToResult(response)
+}
