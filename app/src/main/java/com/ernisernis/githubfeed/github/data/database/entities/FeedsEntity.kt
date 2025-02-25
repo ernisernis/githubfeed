4fb645b1e5b186dfe191d84180da18d3acf6d0de
ernisernis <ernestas.dainys@gmail.com>
2025-02-25 20:52:21 +0200

    Add Feeds entity data class

---
+++ app/src/main/java/com/ernisernis/githubfeed/github/data/database/entities/FeedsEntity.kt
@@ -0,0 +1,30 @@
+package com.ernisernis.githubfeed.github.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class FeedsEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val timelineUrl: String,
+    val userUrl: String,
+    val repositoryDiscussionsUrl: String,
+    val repositoryDiscussionsCategoryUrl: String,
+    val securityAdvisoriesUrl: String,
+    val links: LinksEntity,
+)
+
+@Entity
+data class LinksEntity(
+    val timeLine: LinkEntity,
+    val user: LinkEntity,
+    val repositoryDiscussions: LinkEntity,
+    val repositoryDiscussionsCategory: LinkEntity,
+    val securityAdvisories: LinkEntity
+)
+
+@Entity
+data class LinkEntity(
+    val href: String,
+    val type: String,
+)
