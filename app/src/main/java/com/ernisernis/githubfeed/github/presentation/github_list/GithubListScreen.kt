743bea22093d74d8e560f06e581bf638b0503fbf
ernisernis <ernestas.dainys@gmail.com>
2025-02-28 13:31:55 +0200

    Add ime padding to list screen

--- app/src/main/java/com/ernisernis/githubfeed/github/presentation/github_list/GithubListScreen.kt
+++ app/src/main/java/com/ernisernis/githubfeed/github/presentation/github_list/GithubListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -76,6 +77,7 @@ fun GithubListScreen(
 ) {
     Column(
         modifier = modifier
+            .imePadding()
             .verticalScroll(rememberScrollState())
             .padding(Dimens.GithubListContainerPadding),
         verticalArrangement = Arrangement.spacedBy(Dimens.GithubListContainerItemSpacing)
